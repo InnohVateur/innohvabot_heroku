0915b86f688a6e17a5c41dd927cf31c9a974e2c1
InnohVateur <innohvateur@gmail.com>
2022-04-29 15:23:08 +0200

    Changes were effectued

--- buttons/infos/copy-discord-link.js
+++ buttons/infos/copy-discord-link.js
@@ -1,9 +1,8 @@
-import clipboard from 'clipboardy';
-
+const copy = require('copy-to-clipboard-ultralight');
 module.exports = {
     name : 'copy-discord-link',
     async runInteraction(client, interaction){
-        clipboard.write('https://discord.gg/Nq4wNCsZ2W')
+        copy('https://discord.gg/Nq4wNCsZ2W')
         interaction.reply({content:"Lien d'invitation du discord correctement copié !", ephemeral:true});
     }
 }
