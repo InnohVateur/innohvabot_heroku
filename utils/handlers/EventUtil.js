f87d7136c4e79ed8d7e57abf9f2d55e2d98f37d6
InnohVateur <innohvateur@gmail.com>
2022-05-12 13:55:25 +0200

    EventUtil g rémis le bon handbeurre

--- utils/handlers/EventUtil.js
+++ utils/handlers/EventUtil.js
@@ -4,17 +4,19 @@ const pGlob = promisify(glob);
 const Logger = require('../Logger');
 
 module.exports = async client => {
-    const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+    (await pGlob(`${process.cwd()}/events/*/*.js`)).map(async eventFile => {
+        const event = require(eventFile);
+        if(!event.name) return Logger.warn('Events must have a name')
+        if(!eventList.includes(event.name)) {
+            return Logger.typo(`ERR => unknown event [${eventFile}]`)
+        }
 
-    const eventFiles = fs.readdirSync('./events/*').filter(file => file.endsWith('.js'));
-
-    for (const file of eventFiles) {
-    	const event = require(`./events/*/${file}`);
-    	if (event.once) {
-    		client.once(event.name, (...args) => event.execute(...args));
-    	} else {
-    		client.on(event.name, (...args) => event.execute(...args));
-    	}
-    }
+        Logger.event(`- ${event.name}...`);
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(client, ...args));
+        } else {
+            client.on(event.name, (...args) => event.execute(client, ...args));
+        }
+    })
 }
 const eventList = ['apiRequest','apiResponse','applicationCommandCreateD','applicationCommandDeleteD','applicationCommandUpdateD','channelCreate','channelDelete','channelPinsUpdate','channelUpdate','debug','emojiCreate','emojiDelete','emojiUpdate','error','guildBanAdd','guildBanRemove','guildCreate','guildDelete','guildIntegrationsUpdate','guildMemberAdd','guildMemberAvailable','guildMemberRemove','guildMembersChunk','guildMemberUpdate','guildScheduledEventCreate','guildScheduledEventDelete','guildScheduledEventUpdate','guildScheduledEventUserAdd','guildScheduledEventUserRemove','guildUnavailable','guildUpdate','interactionD','interactionCreate','invalidated','invalidRequestWarning','inviteCreate','inviteDelete','messageD','messageCreate','messageDelete','messageDeleteBulk','messageReactionAdd','messageReactionRemove','messageReactionRemoveAll','messageReactionRemoveEmoji','messageUpdate','presenceUpdate','rateLimit','ready','roleCreate','roleDelete','roleUpdate','shardDisconnect','shardError','shardReady','shardReconnecting','shardResume','stageInstanceCreate','stageInstanceDelete','stageInstanceUpdate','stickerCreate','stickerDelete','stickerUpdate','threadCreate','threadDelete','threadListSync','threadMembersUpdate','threadMemberUpdate','threadUpdate','typingStart','userUpdate','voiceStateUpdate','warn','webhookUpdate'];
